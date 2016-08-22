ad0462ea8aad6e3c27fd5d83284c37b189fbaf9b
Martin Balch <martybalch21@live.com>
2016-08-22 12:01:13 -0700

    I made words

---
+++ Hello AIE/Main.cpp
@@ -0,0 +1,12 @@
+//
+// Name: Martin Balch <martin.balch@students.aie.edu.au>
+//
+
+#include <iostream>
+
+int main()
+{
+	printf("ICE, THERE'S NO ICE, ICE IS JUST A MYTH - Mr.Krabs");
+
+	getchar();
+}
